d5e35bea5ddd8c29eeab7089c25bb55593a7fdd2
Blood <BloodCZE@gmail.com>
2023-12-23 11:37:58 +0100

    button actions

--- Application_Login/login.py
+++ Application_Login/login.py
@@ -3,19 +3,28 @@ from PySide6 import QtCore as qtc
 from PySide6 import QtWidgets as qtw
 from PySide6 import QtGui as qtg
 
-from Application_Login.UI.login_window import Ui_w_LoginForm
-
+from UI.login_window import Ui_w_LoginForm
 
 class LoginForm(qtw.QWidget, Ui_w_LoginForm):
+
+    login_success = qtc.Signal()
+
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
+        self.pb_Cancel.clicked.connect(self.close)
 
+        self.pb_Ok.clicked.connect(self.process_login)
 
+    @qtc.Slot()
+    def process_login(self):
+        if self.le_user_ID.text() == "Jason" and self.le_Password.text() == "Password":
+            self.login_success.emit()
+            self.close()
 
-
-
+        else:
+            self.lb_Message.setText("Login incorrect!")
 
 
 
